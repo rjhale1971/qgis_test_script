e1708163a81b0c1c093c6c64cbbf091ac37b17a0
Sean Brewer <sean@crowdfiber.com>
2022-11-10 11:01:51 -0500

    Actually this works better.

--- test.py
+++ test.py
@@ -2,9 +2,6 @@
 
 import sys
 
-# Inject plugin path, might need to be tweaked significantly. Sure there's more missing.
-sys.path.insert(0, "/Applications/QGIS.app/Contents/Resources/python/plugins")
-
 from qgis.core import (
      QgsApplication, 
      QgsProcessingFeedback, 
@@ -12,14 +9,12 @@ from qgis.core import (
 )
 
 # See https://gis.stackexchange.com/a/155852/4972 for details about the prefix 
-QgsApplication.setPrefixPath('/usr', True)
-#QgsApplication.setPrefixPath("Applications/QGIS-LTR.app/Contents/MacOS",True)
+QgsApplication.setPrefixPath("/Applications/QGIS.app/Contents/MacOS",True)
 qgs = QgsApplication([], False)
 qgs.initQgis()
 
 # Append the path where processing plugin can be found
-sys.path.append('/usr/share/qgis/python/plugins')
-#sys.path.append("Applications/QGIS-LTR.app/Contents/Resources/python/plugins")
+sys.path.append("/Applications/QGIS.app/Contents/Resources/python/plugins")
 
 
 import processing
